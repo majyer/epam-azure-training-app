f7ca5c8445c4375207c02bd467cde4858a6bfcc2
tom.li <Tom_Li@epam.com>
2018-12-14 16:31:37 +0800

    add azure cache code

--- AdventureWorks.Web/Controllers/DepartmentController.cs
+++ AdventureWorks.Web/Controllers/DepartmentController.cs
@@ -4,11 +4,16 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Web.Script.Serialization;
-
+using StackExchange.Redis;
+using System.Configuration;
+using System;
+using System.Diagnostics;
+using Newtonsoft.Json;
 namespace AdventureWorks.Web.Controllers
 {
     public class DepartmentsController : Controller
     {
+        //private const string departmentEmployeecache = "DepartmentEmployee";
         // GET: Departments
         public ActionResult Index()
         {
@@ -31,34 +36,58 @@ namespace AdventureWorks.Web.Controllers
         // GET: Departments/Employees/{id}
         public ActionResult Employees(int id)
         {
-            //DepartmentService departmentService = new DepartmentService();
-            //var departmentEmployees = departmentService.GetDepartmentEmployees(id);
-            //var departmentInfo = departmentService.GetDepartmentInfo(id);
 
-            //ViewBag.Title = "Employees in " + departmentInfo.Name + " Department";
+            var stopwatch = new Stopwatch();
+            IDatabase cache = lazyConnection.Value.GetDatabase();
 
-            //return View(departmentEmployees);
-            string strUrl = "http://epam-azure-training-app.azurewebsites.net/api/DepartmentApi/" + id.ToString();
-            //string strUrl = "http://azure-training-webapps.azurewebsites.net/api/DepartmentApi/" + id.ToString();
-
-            JavaScriptSerializer EmployeeSerializer = new JavaScriptSerializer();
-            var EmployeehttpClient = new HttpClient();
-            var employees = EmployeehttpClient.GetAsync(strUrl).Result.Content.ReadAsStringAsync().Result;
-            List<DepartmentEmployee> departmentEmployees = EmployeeSerializer.Deserialize<List<DepartmentEmployee>>(employees);
-
-            JavaScriptSerializer InfoSerializer = new JavaScriptSerializer();
-            HttpContent InfohttpContent = new StringContent("");
-            InfohttpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            var httpClient = new HttpClient();
-            var Info = httpClient.GetAsync(strUrl).Result.Content.ReadAsStringAsync().Result;
-            //DepartmentInfo departmentInfo = JsonConvert.DeserializeObject(Info).;
-            //DepartmentInfo departmentInfo = EmployeeSerializer.Deserialize<DepartmentInfo>(Info);
-
-
-
-            //ViewBag.Title = "Employees in " + departmentInfo.Name + " Department";
-
+            List<DepartmentEmployee> departmentEmployees =null;
+            DepartmentService departmentService = new DepartmentService();
+            var departmentInfo = departmentService.GetDepartmentInfo(id);
+            stopwatch.Start();
+            var cacheget = cache.StringGet(id.ToString());
+            //var hashes = cache.HashGetAll(id.ToString());
+          
+            if (!cacheget.IsNull)
+            {
+                departmentEmployees = JsonConvert.DeserializeObject<List<DepartmentEmployee>>(cacheget);
+                //departmentEmployees = new List<DepartmentEmployee>();
+                //foreach(DepartmentEmployee hashentry in departmentEmployees)
+                //{
+                //    departmentEmployees.Add(JsonConvert.DeserializeObject<DepartmentEmployee>(hashentry.Value));
+                //}
+                ViewBag.Title = "Employees in cache " + departmentInfo.Name + " Department";
+            }
+            else
+            {
+                string strUrl = "http://epam-azure-training-app.azurewebsites.net/api/DepartmentApi/" + id.ToString();
+               JavaScriptSerializer EmployeeSerializer = new JavaScriptSerializer();
+                var EmployeehttpClient = new HttpClient();
+                var employees = EmployeehttpClient.GetAsync(strUrl).Result.Content.ReadAsStringAsync().Result;
+                departmentEmployees = EmployeeSerializer.Deserialize<List<DepartmentEmployee>>(employees);
+                cache.StringSet(id.ToString(), JsonConvert.SerializeObject(departmentEmployees));
+                //foreach(DepartmentEmployee de in departmentEmployees)
+                //{
+                //    cache.StringSet(de.Id.ToString(), JsonConvert.SerializeObject(de));
+                //}
+                ViewBag.Title = "Employees in url" + departmentInfo.Name + " Department";
+            }
+            stopwatch.Stop();
+            ViewBag.Message = "MS " + stopwatch.ElapsedMilliseconds;
+            //lazyConnection.Value.Dispose();
             return View(departmentEmployees);
+        }
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
         }
     }
 }
